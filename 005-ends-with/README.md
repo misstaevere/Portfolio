e5f425485a34e3ba1549b8faec1c9d32c7ca9930
Chris Perrins <chrisperrins95@gmail.com>
2020-05-27 22:52:55 +0100

    005 ends with task added

---
+++ 005-ends-with/README.md
@@ -0,0 +1,4 @@
+# Ends With
+
+## Overview
+Create a method which checks if a sentence ends with "java". Ignore case
---
+++ 005-ends-with/src/main/java/com/qa/portfolio/EndsWith.java
@@ -0,0 +1,16 @@
+package com.qa.portfolio;
+
+public class EndsWith {
+	
+	/**
+	 * Returns true if it the sentence ends in "java" 
+	 * Ignores case
+	 * 
+	 * @param sentence which may or may not end in java
+	 * @return true is sentence ends in java or it will return false
+	 */
+	public boolean endsJava(String sentence) {
+		return false;
+	}
+
+}
