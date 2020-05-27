14ba9d29b4cc8b72e081ffc95f21aa961908a1e9
Kart Taevere <kartaevere@gmail.com>
2020-05-27 17:29:27 +0100

    001 resolved

--- 001-multichar/src/main/java/com/qa/portfolio/MultiChar.java
+++ 001-multichar/src/main/java/com/qa/portfolio/MultiChar.java
@@ -1,18 +1,21 @@
 package com.qa.portfolio;
 
-
 public class MultiChar {
 
 	/**
-	 * For each character in the entered string the method will triplicate
-	 * each the character and return the concatenated string.
+	 * For each character in the entered string the method will triplicate each the
+	 * character and return the concatenated string.
 	 * 
 	 * @param input - any string
 	 * @return triplicates of each letter from the input
 	 */
 	public String multiChar(String input) {
-		// TODO
-		return "";
+		int len = input.length();
+		String newstring = "";
+		for (int i = 0; i < len; i++) {
+			newstring += input.substring(i, i + 1) + input.substring(i, i + 1) + input.substring(i, i + 1);
+		}
+		return newstring;
 	}
 
 }
