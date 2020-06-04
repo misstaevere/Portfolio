26358ef455d2c2b0766995e990b65bb839bc49e3
Kart Taevere <kartaevere@gmail.com>
2020-06-04 17:55:45 +0100

    007 finished

--- 007-super-block/src/main/java/com/qa/portfolio/SuperBlock.java
+++ 007-super-block/src/main/java/com/qa/portfolio/SuperBlock.java
@@ -3,12 +3,32 @@ package com.qa.portfolio;
 public class SuperBlock {
 
 	/**
-	 * Return the largest length of consecutively repeating characters, do not ignore case
+	 * Return the largest length of consecutively repeating characters, do not
+	 * ignore case
 	 * 
-	 * @param input 
+	 * @param input
 	 * @return the largest number of consecutive characters
 	 */
 	public int superBlock(String input) {
-    	return -1;
+
+		if (input.isEmpty()) {
+			return 0;
+		}
+		int longest = 0;
+		int length = 1;
+		for (int i = 0; i < input.length() - 1; i++) {
+			if (input.charAt(i) == input.charAt(i + 1)) {
+				++length;
+				if (length > longest) {
+					longest = length;
+				}
+			} else {
+				length = 1;
+			}
+		}
+		if (length > longest) {
+			longest = length;
+		}
+		return longest;
 	}
 }
